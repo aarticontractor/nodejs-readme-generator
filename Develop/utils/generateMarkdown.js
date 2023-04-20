1162f1a660504edb359e0a0285dbbe7018a4c4b9
aarticontractor <contaaru@gmail.com>
2023-04-19 22:29:21 -0700

    created switch statement to render license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,23 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODO: Create a function that returns a license badge based on which license is passed
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Here I have used the switch case statement in the renderLicenseBadge function to render the license badge
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT License":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "Apache License 2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+    case "GNU General Puplic License v3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+    case "Mozilla Public License 2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+    case "None":
+      return "";
+    default:
+      return "";
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
